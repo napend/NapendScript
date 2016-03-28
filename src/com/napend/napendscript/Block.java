c0eff67f5a69583659ce4ef889e58477a733ae7a
alexmunoz905 <alex.munoz905@gmail.com>
2016-03-28 19:09:47 -0400

    Added the block element

--- src/com/napend/napendscript/Block.java
+++ src/com/napend/napendscript/Block.java
@@ -3,14 +3,23 @@ package com.napend.napendscript;
 import java.util.ArrayList;
 
 /**
- * 
  * Represents a block of code.
- *
  */
 
-public class Block {
+public abstract class Block {
 
 	private Block superBlock;
 	private ArrayList<Block> subBlocks;
+	
+	public Block(Block superBlock){
+		this.superBlock = superBlock;
+		this.subBlocks = new ArrayList<>();
+	}
+	
+	public Block getSuperBlock() {
+		return superBlock;
+	}
+	
+	public abstract void run();
 	
 }
